dc69c50885f6393e7597cc86c7de5fa903b96751
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-22 20:20:48 +0300

    Add method showList

--- src/app/task1/Main.java
+++ src/app/task1/Main.java
@@ -8,4 +8,14 @@ public class Main {
         return new ArrayList<>(Arrays.asList("John","Alice","Robert",
                 "Lise","David","Amanda"));
     }
+
+    public static String showList(ArrayList<String> list){
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        for (String name : list) {
+            count++;
+            result.append(count).append(") ").append(name).append(" \n");
+        }
+        return result.toString();
+    }
 }
