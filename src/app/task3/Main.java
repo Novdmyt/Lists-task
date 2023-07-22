c976593387cecfd9d565fb8c2f5bc0985653f82e
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-22 20:39:22 +0300

    Add method getData

---
+++ src/app/task3/Main.java
@@ -0,0 +1,11 @@
+package app.task3;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static LinkedList<String> getData(){
+        return new LinkedList<>(Arrays.asList("orange", "grape", "apple","lemon"));
+    }
+}
