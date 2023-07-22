4f8373f5cae4180fd543daa21385257ec67fba70
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-22 20:17:23 +0300

    Add method getData

---
+++ src/app/task1/Main.java
@@ -0,0 +1,11 @@
+package app.task1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static ArrayList<String> getData(){
+        return new ArrayList<>(Arrays.asList("John","Alice","Robert",
+                "Lise","David","Amanda"));
+    }
+}
