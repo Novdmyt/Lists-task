c8c628eeb2f6b54f6800009ce1fabb279155d276
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-22 20:29:52 +0300

    Add method getData

---
+++ src/app/task2/Main.java
@@ -0,0 +1,13 @@
+package app.task2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static List<String> getData(){
+        return new ArrayList<>(Arrays.asList("orange", "mango","banana",
+                "apple","kiwi","cherry"));
+    }
+}
